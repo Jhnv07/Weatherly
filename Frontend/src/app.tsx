5f28b33a08622ba2adf58be3ce78eaf8ef5c3f95
Jhnv07 <jahnavi.yadav007@gmail.com>
2025-07-11 12:22:28 +0530

    app.tsx updated

--- Frontend/src/app.tsx
+++ Frontend/src/app.tsx
@@ -36,7 +36,7 @@ function App() {
     // }, 1000);
 
     try {
-      const response = await fetch("/api", {
+      const response = await fetch("http://localhost:5000/api", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
